cace5892511b05f8a937c571a90e563557757216
Martin Monkov <134950476+mbmonkov@users.noreply.github.com>
2024-07-24 16:45:07 +0300

    Update Teacher.cpp

--- 10Week/Person, Student, Teacher/Teacher.cpp
+++ 10Week/Person, Student, Teacher/Teacher.cpp
@@ -1,7 +1,7 @@
 #include "Teacher.h"
 #pragma warning (disable:4996)
 
-// Копира динамичен масив от низове
+// РљРѕРїРёСЂР° РґРёРЅР°РјРёС‡РµРЅ РјР°СЃРёРІ РѕС‚ РЅРёР·РѕРІРµ
 static char** copyArrayOfString(const char* const* strings, size_t size)
 {
     char** res = new char* [size];
@@ -15,7 +15,7 @@ static char** copyArrayOfString(const char* const* strings, size_t size)
     return res;
 }
 
-// Освобождава динамично заделената памет за масив от низове
+// РћСЃРІРѕР±РѕР¶РґР°РІР° РґРёРЅР°РјРёС‡РЅРѕ Р·Р°РґРµР»РµРЅР°С‚Р° РїР°РјРµС‚ Р·Р° РјР°СЃРёРІ РѕС‚ РЅРёР·РѕРІРµ
 static void freeArraysOfString(char** strings, unsigned stringsCount)
 {
     for (size_t i = 0; i < stringsCount; i++)
@@ -23,72 +23,72 @@ static void freeArraysOfString(char** strings, unsigned stringsCount)
     delete[] strings;
 }
 
-// Конструктор на Teacher, инициализира с име, възраст, и списък с теми
+// РљРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ РЅР° Teacher, РёРЅРёС†РёР°Р»РёР·РёСЂР° СЃ РёРјРµ, РІСЉР·СЂР°СЃС‚, Рё СЃРїРёСЃСЉРє СЃ С‚РµРјРё
 Teacher::Teacher(const char* name, int age, const char* const* subjects, size_t subjectsCount) : Person(name, age)
 {
     this->subjects = copyArrayOfString(subjects, subjectsCount);
     this->subjectsCount = subjectsCount;
 }
 
-// Освобождава динамично заделената памет за темите
+// РћСЃРІРѕР±РѕР¶РґР°РІР° РґРёРЅР°РјРёС‡РЅРѕ Р·Р°РґРµР»РµРЅР°С‚Р° РїР°РјРµС‚ Р·Р° С‚РµРјРёС‚Рµ
 void Teacher::free()
 {
     freeArraysOfString(subjects, subjectsCount);
 }
 
-// Копира данните от друг Teacher
+// РљРѕРїРёСЂР° РґР°РЅРЅРёС‚Рµ РѕС‚ РґСЂСѓРі Teacher
 void Teacher::copyFrom(const Teacher& other)
 {
     subjects = copyArrayOfString(other.subjects, other.subjectsCount);
     subjectsCount = other.subjectsCount;
 }
 
-// Конструктор за копиране, копира всички член данни от друг Teacher
+// РљРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ Р·Р° РєРѕРїРёСЂР°РЅРµ, РєРѕРїРёСЂР° РІСЃРёС‡РєРё С‡Р»РµРЅ РґР°РЅРЅРё РѕС‚ РґСЂСѓРі Teacher
 Teacher::Teacher(const Teacher& other) : Person(other)
 {
-    copyFrom(other); // само частта от Teacher се копира
+    copyFrom(other); // СЃР°РјРѕ С‡Р°СЃС‚С‚Р° РѕС‚ Teacher СЃРµ РєРѕРїРёСЂР°
 }
 
-// Оператор за присвояване чрез копиране
+// РћРїРµСЂР°С‚РѕСЂ Р·Р° РїСЂРёСЃРІРѕСЏРІР°РЅРµ С‡СЂРµР· РєРѕРїРёСЂР°РЅРµ
 Teacher& Teacher::operator=(const Teacher& other)
 {
     if (this != &other)
     {
-        Person::operator=(other); // първо копира данните от Person
-        free(); // освобождава старите ресурси
-        copyFrom(other); // копира новите ресурси
+        Person::operator=(other); // РїСЉСЂРІРѕ РєРѕРїРёСЂР° РґР°РЅРЅРёС‚Рµ РѕС‚ Person
+        free(); // РѕСЃРІРѕР±РѕР¶РґР°РІР° СЃС‚Р°СЂРёС‚Рµ СЂРµСЃСѓСЂСЃРё
+        copyFrom(other); // РєРѕРїРёСЂР° РЅРѕРІРёС‚Рµ СЂРµСЃСѓСЂСЃРё
     }
     return *this;
 }
 
-// Премества данните от друг Teacher
+// РџСЂРµРјРµСЃС‚РІР° РґР°РЅРЅРёС‚Рµ РѕС‚ РґСЂСѓРі Teacher
 void Teacher::moveFrom(Teacher&& other)
 {
     subjects = other.subjects;
     subjectsCount = other.subjectsCount;
-    other.subjects = nullptr; // освободете ресурса в другия обект
+    other.subjects = nullptr; // РѕСЃРІРѕР±РѕРґРµС‚Рµ СЂРµСЃСѓСЂСЃР° РІ РґСЂСѓРіРёСЏ РѕР±РµРєС‚
     other.subjectsCount = 0;
 }
 
-// Конструктор за преместване, инициализира с преместените ресурси
+// РљРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ Р·Р° РїСЂРµРјРµСЃС‚РІР°РЅРµ, РёРЅРёС†РёР°Р»РёР·РёСЂР° СЃ РїСЂРµРјРµСЃС‚РµРЅРёС‚Рµ СЂРµСЃСѓСЂСЃРё
 Teacher::Teacher(Teacher&& other) noexcept : Person(std::move(other))
 {
     moveFrom(std::move(other));
 }
 
-// Оператор за присвояване чрез преместване
+// РћРїРµСЂР°С‚РѕСЂ Р·Р° РїСЂРёСЃРІРѕСЏРІР°РЅРµ С‡СЂРµР· РїСЂРµРјРµСЃС‚РІР°РЅРµ
 Teacher& Teacher::operator=(Teacher&& other) noexcept
 {
     if (this != &other)
     {
-        Person::operator=(std::move(other)); // премества данните от Person
-        free(); // освобождава старите ресурси
-        moveFrom(std::move(other)); // премества новите ресурси
+        Person::operator=(std::move(other)); // РїСЂРµРјРµСЃС‚РІР° РґР°РЅРЅРёС‚Рµ РѕС‚ Person
+        free(); // РѕСЃРІРѕР±РѕР¶РґР°РІР° СЃС‚Р°СЂРёС‚Рµ СЂРµСЃСѓСЂСЃРё
+        moveFrom(std::move(other)); // РїСЂРµРјРµСЃС‚РІР° РЅРѕРІРёС‚Рµ СЂРµСЃСѓСЂСЃРё
     }
     return *this;
 }
 
-// Деструктор, освобождава динамично заделената памет
+// Р”РµСЃС‚СЂСѓРєС‚РѕСЂ, РѕСЃРІРѕР±РѕР¶РґР°РІР° РґРёРЅР°РјРёС‡РЅРѕ Р·Р°РґРµР»РµРЅР°С‚Р° РїР°РјРµС‚
 Teacher::~Teacher()
 {
     free();
